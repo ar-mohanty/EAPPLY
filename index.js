96570d2db491431181cbacc1f514bfc0ba7b075e
Anurag Mohanty <68963714+AR-Mohanty@users.noreply.github.com>
2023-10-12 23:32:14 +0530

    client url added

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ console.log("hello world");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
