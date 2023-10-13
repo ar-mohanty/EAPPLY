87fa9b34492de249ed5f87e5324fb232d6f3bd81
Anurag Mohanty <68963714+AR-Mohanty@users.noreply.github.com>
2023-10-13 23:54:44 +0530

    Update passport.js

--- passport.js
+++ passport.js
@@ -4,13 +4,12 @@ const GithubStrategy = require("passport-github2").Strategy;
 
 const passport = require("passport");
 
-console.log(process.env.GOOGLE_CLIENT_ID);
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientID: process.env['GOOGLE_CLIENT_ID'],
+      clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
       callbackURL: "https://eapply-backend.vercel.app/auth/google/callback",
     },
     function (accessToken, refreshToken, profile, done) {
@@ -22,8 +21,8 @@ passport.use(
 passport.use(
   new GithubStrategy(
     {
-      clientID: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientID: process.env['GITHUB_CLIENT_ID'],
+      clientSecret: process.env['GITHUB_CLIENT_SECRET'],
       callbackURL: "https://eapply-backend.vercel.app/auth/github/callback",
     },
     function (accessToken, refreshToken, profile, done) {
