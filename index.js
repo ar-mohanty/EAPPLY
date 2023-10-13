931dfc5095376c5d72eaf9b7ed67a1df9fed3ac8
Anurag Mohanty <68963714+AR-Mohanty@users.noreply.github.com>
2023-10-13 11:46:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "https://eapply-client.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
