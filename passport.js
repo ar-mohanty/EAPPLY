a4d0354ac5640954cb3fa83a75ad148c4e42ea8a
Anurag Mohanty <68963714+AR-Mohanty@users.noreply.github.com>
2023-10-13 11:31:05 +0530

    Update passport.js

--- passport.js
+++ passport.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://eapply-backend.vercel.app/auth/google/callback",
     },
     function (accessToken, refreshToken, profile, done) {
       done(null, profile);
